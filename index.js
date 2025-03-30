5087ca288368bfc3e3a54b082851821a5a8f97f7
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-03-29 20:45:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ dotenv.config();
 const PORT = 5000;
 
 app.listen(PORT, () => {
+
+  console.log("RabbitMQ Config:");
+  console.log("Host:", process.env.RABBITMQ_HOST);
+  console.log("User:", process.env.RABBITMQ_USER);
+  console.log("Pass:", process.env.RABBITMQ_PASS);
+  console.log("VHost:", process.env.RABBITMQ_USER);
+  
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
