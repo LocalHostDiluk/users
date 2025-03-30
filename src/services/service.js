6c851ce1995bbf8c5d5e6ec7c05b254ccec873bf
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-03-29 20:47:25 -0600

    Update service.js

--- src/services/service.js
+++ src/services/service.js
@@ -8,32 +8,38 @@ const RABBITMQ_EXCHANGE = "user_event";
 const RABBITMQ_ROUTING_KEY = "user.created";
 
 export async function userCreatedEvent(user) {
-  const connection = await amqp.connect({
-    protocol: "amqps",  // Cambia de "amqp" a "amqps"
-    hostname: process.env.RABBITMQ_HOST,
-    port: 5671,  // Usa el puerto para TLS
-    username: process.env.RABBITMQ_USER,
-    password: process.env.RABBITMQ_PASS,
-    vhost: process.env.RABBITMQ_VHOST,
-  });
-  const channel = await connection.createChannel();
-
-  //Declare exchange
-  await channel.assertExchange(RABBITMQ_EXCHANGE, "topic", { durable: true });
-
-  //Publicar el evento
-  const message = JSON.stringify(user);
-  channel.publish(
-    RABBITMQ_EXCHANGE,
-    RABBITMQ_ROUTING_KEY,
-    Buffer.from(message)
-  );
-
-  console.log(
-    `[x] exchange "${RABBITMQ_EXCHANGE}", routing key "${RABBITMQ_ROUTING_KEY}": ${message}`
-  );
-
-  setTimeout(() => {
-    connection.close();
-  }, 500);
+  try{
+    const connection = await amqp.connect({
+      protocol: "amqps",  // Cambia de "amqp" a "amqps"
+      hostname: process.env.RABBITMQ_HOST,
+      port: 5671,  // Usa el puerto para TLS
+      username: process.env.RABBITMQ_USER,
+      password: process.env.RABBITMQ_PASS,
+      vhost: process.env.RABBITMQ_VHOST,
+    });
+    const channel = await connection.createChannel();
+  
+    //Declare exchange
+    await channel.assertExchange(RABBITMQ_EXCHANGE, "topic", { durable: true });
+  
+    //Publicar el evento
+    const message = JSON.stringify(user);
+    channel.publish(
+      RABBITMQ_EXCHANGE,
+      RABBITMQ_ROUTING_KEY,
+      Buffer.from(message)
+    );
+  
+    console.log(
+      `[x] exchange "${RABBITMQ_EXCHANGE}", routing key "${RABBITMQ_ROUTING_KEY}": ${message}`
+    );
+  
+    setTimeout(() => {
+      connection.close();
+    }, 500);
+  } catch (error) {
+    console.error("Error conectando a RabbitMQ:", error.message);
+    console.error("Reintentando en 5s...");
+    setTimeout(userEvents, 5000);
+  }
 }
