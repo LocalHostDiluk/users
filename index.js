fe83f2048ac81900d66d39c651f0bbcda65e38b1
LocalHostDiluk <sebas.lozano.reyes@gmail.com>
2025-03-31 16:30:56 -0600

    owiew

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ dotenv.config();
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
+app.use(cookieParser());
 
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:8081/", // Cambia esto según tu frontend
+    credentials: true, // Permite el envío de cookies de autenticación
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", routes);
+
+app.listen(PORT, () => {
   console.log("RabbitMQ Config:");
   console.log("Host:", process.env.RABBITMQ_HOST);
   console.log("User:", process.env.RABBITMQ_USER);
   console.log("Pass:", process.env.RABBITMQ_PASS);
   console.log("VHost:", process.env.RABBITMQ_USER);
-  
+
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
